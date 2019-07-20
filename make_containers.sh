920cd24ebaa24a87cd5eaa684b9e150f4156b678
Matt Heller <mjheller414@gmail.com>
2019-07-20 11:29:53 -0500

    Update make_containers.sh

--- make_containers.sh
+++ make_containers.sh
@@ -22,7 +22,7 @@ cd ~/bert
 
 CLIENT_IMAGE_NAME=bert_agnews_client
 CLIENT_VER=v3
-DOCKER_USER=poseidon
+DOCKER_USER=p0seid0n
 mkdir asset
 gsutil cp gs://cloud-tpu-checkpoints/bert/uncased_L-12_H-768_A-12/vocab.txt asset/
 docker build -t $USER/$CLIENT_IMAGE_NAME .
